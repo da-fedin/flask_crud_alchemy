b353807aef8d9b0569cea879b7ce94f62980e0e9
da-fedin <tm.i.sm.master@gmail.com>
2024-04-18 12:53:34 +0300

    init

---
+++ scripts/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Build started"
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install dependencies using pip
+pip install -r requirements.txt
+
+# Deactivate the virtual environment
+deactivate
+
+echo "Build completed"
